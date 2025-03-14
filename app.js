d277d665809b3fbd0e040f95d15a93812ccf1c91
torip <torhel02840@stud.noroff.no>
2025-03-14 13:51:54 +0100

    chore: typo

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const SECRET = process.env.SECRET_KEY;
 const jwt = require("jsonwebtoken");
 
 app.use(express.json()); //Parse JSON bodies
-app.use("/todo", todoRouter); //We only have 1 main edpoint, so we set up a router to avoid repeating ourselves.
+app.use("/todo", todoRouter); //We only have 1 main endpoint, so we set up a router to avoid repeating ourselves.
 
 app.get("/", (req, res) => res.redirect("/todo")); //Our main endpoint is not being used, redirect straight to /todo
 

97c82164a4a223808e5150cf33741dc04a393263
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-03-14 19:32:33 +0100

    refactor: improve documentation for swagger

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const populateDB = require('./populateTodoDb');
-
 const todoRouter = require('./routes/todoRoutes.js');
-
 const PORT = process.env.PORT || 3000;
 const SECRET = process.env.SECRET_KEY;
 const jwt = require('jsonwebtoken');
-
 const swagger = require('swagger-ui-express');
-const openapi = require('yamljs').load('./openapi.yaml');
 
+const openapi = require('yamljs').load('./openapi.yaml');
 const token = jwt.sign({ user: 'sys', role: 'admin' }, SECRET, { expiresIn: '1h' }); //Token is created at server start and expires in 1h
 
 app.use('/docs', swagger.serve, swagger.setup(openapi));
+app.use('/assets', express.static('public/assets'))
 
 app.use(express.json()); //Parse JSON bodies
 app.use('/todo', todoRouter); //We only have 1 main endpoint, so we set up a router to avoid repeating ourselves.

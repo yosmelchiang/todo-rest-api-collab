d7c7fc0489a0ee17d56e97402c525118600be70b
torip <torhel02840@stud.noroff.no>
2025-03-14 12:11:19 +0100

    feat: populated the todoService.js file

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,9 +2,16 @@ const express = require('express');
 const { getTodos, getTodo, createTodo, deleteTodo } = require ('../services/todoService');
 const router = express.Router();
 
-router.get('/', getTodos);
+// Gets all todo items 
+router.get('/', getTodos); 
+
+// Gets a single todo item by id
 router.get('/:id', getTodo);
+
+// Creates a new todo item 
 router.post('/', createTodo);
+
+// Deletes a todo item by id 
 router.delete('/:id', deleteTodo);
 
 module.exports = router; 
---
+++ services/todoService.js
@@ -0,0 +1,32 @@
+const Todo = require('../models/Todo');
+
+// Retrieve all TODO items 
+exports.getTodos = async (req, res) => res.json(await Todo.findAll());
+
+// Retrieve a single TODO item by ID
+exports.getTodo = async (req, res) => {
+    const todo = await Todo.findByPk(req.params.id);
+    if (!todo) return res.status(404).json({ message: 'Not found' });
+    res.json(todo);
+};
+
+// Creates a new TODO item
+exports.createTodo = async (req, res) => {
+    const { name, deadline, points } = req.body;
+    try {
+        const newTodo = await Todo.create({ name, deadline, points });
+        res.status(201).json(newTodo);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Deletes a TODO item by ID 
+exports.deleteTodo = async (req, res) => {
+    await Todo.destroy({ where: { id: req.params.id} });
+    res.json({ message: 'Deleted successfully' });
+
+};
+
+
+module.exports = { getTodos, getTodo, createTodo, deleteTodo };

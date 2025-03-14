9c3e6fde8c488c9a33ea22a76d1a464dceb74635
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-03-14 12:30:51 +0100

    fix: correct env variables

--- config/db.js
+++ config/db.js
@@ -7,9 +7,10 @@
 //     dialect: process.env.DB_DIALECT,
 //   }
 // );
+require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
 });

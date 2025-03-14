253988f48b21ffe409e257d8969f420059ce3fb2
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-03-14 12:10:17 +0100

    chore: add delete todo route logic

--- app.js
+++ app.js
@@ -31,7 +31,23 @@ router.post('/', (req, res) => {
   res.status(201).json({ status: 'succes', message: 'Created a new todo', data: todo });
 });
 
-// router.delete
+router.delete('/:id', (req, res) => {
+  const dummyDB = [{ id: 1, name: 'Walk the dog', deadline: '14.03.2025', points: 123 }]; //Just simulate a DB to create some error scenarios
+  const id = Number(req.params.id); //its a string, we need to convert it to a number
+  const todo = dummyDB.find((e) => e.id === id);
+
+  //If we cant find a todo, return with a code and a error message
+  if (!todo) {
+    return res.status(404).json({ status: 'fail', message: 'Todo not found' });
+  }
+
+  //If we get here, means we have a todo, delete it from a database and confirm
+  const db = dummyDB.filter((e) => e.id !== id); //supposed to simulate a database deletion
+
+  res
+    .status(200)
+    .json({ status: 'success', message: `Successfully deleted todo with id ${id}`, data: db });
+});
 
 app.listen(PORT, () => {
   console.log(`App is up on http://localhost:${PORT}`);

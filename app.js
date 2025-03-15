5272b63ec48b946fba60bfa45f5688ec6b6c21cd
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-03-15 08:58:56 +0100

    feat: add login athentication with /login to generate tokens

--- app.js
+++ app.js
@@ -9,18 +9,29 @@ const jwt = require('jsonwebtoken');
 const swagger = require('swagger-ui-express');
 
 const openapi = require('yamljs').load('./openapi.yaml');
-const token = jwt.sign({ user: 'sys', role: 'admin' }, SECRET, { expiresIn: '1h' }); //Token is created at server start and expires in 1h
 
 app.use('/docs', swagger.serve, swagger.setup(openapi));
-app.use('/assets', express.static('public/assets'))
 
 app.use(express.json()); //Parse JSON bodies
 app.use('/todo', todoRouter); //We only have 1 main endpoint, so we set up a router to avoid repeating ourselves.
 
-app.get('/', (req, res) => res.redirect('/todo')); //Our main endpoint is not being used, redirect straight to /todo
+app.get('/', (req, res) => res.redirect('/docs')); //Our main endpoint is not being used, redirect straight to /docs
 
 populateDB();
 
+// Login route for generating JWT
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Instead of hardcodng we should connect it against a DB user
+  if (username === 'sys' && password === 'password123') {  // Example hardcoded check
+    const token = jwt.sign({ user: 'sys', role: 'admin' }, SECRET, { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } else {
+    res.status(401).json({ message: 'Invalid Credentials' });
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`App is up on http://localhost:${PORT} - a 1h authorization token was generated: ${token}`);
+  console.log(`App is up on http://localhost:${PORT}`);
 });

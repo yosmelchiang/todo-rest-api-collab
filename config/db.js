d8d7a6944d265a04cf1d78ad3739aca552d1c814
David Instefjord <davidinstefjord@gmail.com>
2025-03-31 17:47:51 +0200

    Chore: edited db.js

--- config/db.js
+++ config/db.js
@@ -4,14 +4,19 @@
 //   process.env.DB_PASSWORD,
 //   {
 //     host: process.env.DB_HOST,
-//     dialect: process.env.DB_DIALECT,
+//      dialect: process.env.DB_DIALECT,
 //   }
 // );
-require('dotenv').config();
+require("dotenv").config();
 
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-});
+    dialect: process.env.DB_DIALECT,
+  }
+);
 module.exports = sequelize;
